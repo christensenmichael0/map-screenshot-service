c1f683626a1e2c2bb8ef93f0dd6a332d1574dff5
Michael Christensen <christensenmichael0@gmail.com>
2020-12-04 10:33:46 -0500

    start thinking about animation workflow (#4)

--- scripts/generateAnimation.js
+++ scripts/generateAnimation.js
@@ -7,7 +7,17 @@ const axios = require('axios');
 // maybe wait for basemap and legend to be fetched and built before moving forward
 
 async function generateAnimation (layers, dateTimeRange, dimensions, outputFormat) {
-    // for each layer need to determine the available times between start/end by looking at getCaps
+    // layers array has a length equal to the number of layers on the map being animated
+    // each layer object must contain a key: map_times which will be an array of times,
+    // additionally key: valid_times should be provided and container an array of times equal in length
+    // to the aforementioned map_times. Map times are a reflection the the timeslider ticks
+    // provided by the application UI. If more than 2 layers are being animated
+    // then each of these layers will contain the same value for map_times. The map_time is
+    // the frame time so it shouldn't change. The valid_times, however, could change. For any
+    // map time an individual layer will have its own valid time. All of this needs to be resolved
+    // outside of this service. The job of this service is to assemble WMS data and for future
+    // maintainability and adoption it should do ONLY this. Other tasks are outside the scope of this
+    // project and should have utilize a separate service.
 
     // TODO: function -- assembleLayerData
     // assemble packet of information needed to generate an individual image file
