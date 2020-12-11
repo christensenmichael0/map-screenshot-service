4189ed3f25b47ab76b68817edebb7b6694176cff
Michael Christensen <christensenmichael0@gmail.com>
2020-12-11 16:01:40 -0500

    fix job-status and download routes (#12)

--- routes/download.js
+++ routes/download.js
@@ -1,12 +1,22 @@
 const express = require ('express');
 const {getObject} = require('../services/aws');
+const {JobStatus} = require('../services/MongoServices');
 
 const app = express();
 
 // get file from s3
 app.get('/:id', async (req, res) => {
+
+    let jobInfo;
     try {
-        let {contentType, data} = await getObject(key);
+        jobInfo = await JobStatus.findById(req.params.id);
+    } catch (err) {
+        res.send('resource not found').status(404);
+    }
+
+    let {objectKey} = jobInfo;
+    try {
+        let {contentType, data} = await getObject(objectKey);
         res.contentType(contentType);
         res.send(data);
     } catch (err) {
